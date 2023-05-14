3bd0bd47110270c29901caf01c6329d7253fc270
Gaby <paogaby0803@gmail.com>
2023-05-14 16:20:48 -0500

    chore: example comprehension Dictionary convert F to C

--- basics/2. Dic Comprehension.py
+++ basics/2. Dic Comprehension.py
@@ -4,5 +4,10 @@ x = dict_numbers.keys()
 square_dict = {k: v**2 for (k, v) in dict_numbers.items() if 'A' in k}
 print(square_dict)
 
+temp_fahrenheit = {"temp_1": 10, "temp_2": 9, "temp_3": 50, "temp_4": 45}
+temp_celsius = {k: ((v-32)*5)/9 for k, v in temp_fahrenheit.items()}
+print(temp_celsius)
+
+
 
 
